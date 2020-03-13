28309eb4ac9807a4670824852ed3d9cbe89e6fbf
Polina Voytenko <astrolabus8848@gmail.com>
2020-03-14 01:18:08 +0500

    add cs-hw 1

--- VK_client/VK_client/LoginFormController.swift
+++ VK_client/VK_client/LoginFormController.swift
@@ -77,6 +77,7 @@ class LoginFormController: UIViewController {
         } else {
             return false
         }
+        
     }
     
     func showLoginError() {
---
+++ VK_client/VK_client/Session Info/Session.swift
@@ -0,0 +1,17 @@
+//
+//  Session.swift
+//  VK_client
+//
+//  Created by Полина Войтенко on 13.03.2020.
+//  Copyright © 2020 Полина Войтенко. All rights reserved.
+//
+import UIKit
+
+class Session {
+    static let shared: Session = .init()
+    
+    private init() {}
+    
+    var token = ""
+    var userID: Int = 0
+}
